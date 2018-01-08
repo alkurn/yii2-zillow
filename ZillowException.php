0bd53cdafefae443b2d167bf1f66f409e3c0efce
ganesh alkurn <ganesh.alkurn@gmail.com>
2018-01-08 11:20:24 +0530

    Initial commit

---
+++ ZillowException.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the Zillow package.
+ *
+ * (c) Ganesh Alkurn <ganesh.alkurn@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace alkurn\zillow;
+
+/**
+ * Default exception class
+ */
+class ZillowException extends \Exception {}
